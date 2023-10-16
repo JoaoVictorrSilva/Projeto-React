6da447ff6477c934f038889702bfb44ccb116657
João Victor da Silva <joao.vt2807@gmail.com>
2023-10-16 10:24:14 -0300

    Update Form.jsx

--- project-react/src/components/Form.jsx
+++ project-react/src/components/Form.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
+
 function Form ( ) {
 
   function cadastrarUsuario (e) {
     e.preventDefault();
+    console.log(name, password);
     console.log("Cadastrou o usuário!");
   }
+
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
 
   return ( 
     <div>
       <form onSubmit={cadastrarUsuario}>
         <div>
-          <input type="text" placeholder="Digite o seu nome:"/>
+          <label htmlFor="name">Nome:</label>
+            <input 
+              type="text" 
+              id="name" 
+              name="name" 
+              placeholder="Digite o seu nome"
+              onChange={(e) => setName(e.target.value)}
+            />
         </div>
+
         <div>
-          <input type="submit" value="Cadastrar:"/>
+          <label htmlFor="password">Senha:</label>
+            <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              placeholder="Digite a sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+
+        <div>
+          <input type="submit" value="Cadastrar"/>
         </div>
       </form>
     </div>
